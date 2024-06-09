7ff2cc338a597eae6fe68e37fa30af73936656a2
woodyjohnson13 <johnsonwoody13@gmail.com>
2024-06-09 22:12:21 +0400

    ui fixes

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -28,46 +28,47 @@ function Selector({ scheduleType, setScheduleType }) {
   return (
     <div className='selector'>
       <h3 className='selector_title'>График</h3>
-      
-      <label>
-        <input 
-          type="radio" 
-          value="weekly"
-          id="week" 
-          checked={scheduleType === 'weekly'} 
-          onChange={() => setScheduleType('weekly')} 
-        />
-        <span>Дни недели</span>
-      </label>
+      <div className='selector_container'>
+        <label>
+          <input 
+            type="radio" 
+            value="weekly"
+            id="week" 
+            checked={scheduleType === 'weekly'} 
+            onChange={() => setScheduleType('weekly')} 
+          />
+          <span>Дни недели</span>
+        </label>
 
-      <label>
-        <input 
-          type="radio" 
-          value="daily" 
-          checked={scheduleType === 'daily'} 
-          onChange={() => setScheduleType('daily')} 
-        />
-        <span>Ежедневно</span>
-      </label>
+        <label>
+          <input 
+            type="radio" 
+            value="daily" 
+            checked={scheduleType === 'daily'} 
+            onChange={() => setScheduleType('daily')} 
+          />
+          <span>Ежедневно</span>
+        </label>
 
-      <label>
-        <input 
-          type="radio" 
-          value="minutes" 
-          checked={scheduleType === 'minutes'} 
-          onChange={() => setScheduleType('minutes')} 
-        />
-        <span>Минуты</span>
-      </label>
-      <label>
-        <input 
-          type="radio" 
-          value="monthly" 
-          checked={scheduleType === 'monthly'} 
-          onChange={() => setScheduleType('monthly')} 
-        />
-        <span>Месяцы</span>
-      </label>
+        <label>
+          <input 
+            type="radio" 
+            value="minutes" 
+            checked={scheduleType === 'minutes'} 
+            onChange={() => setScheduleType('minutes')} 
+          />
+          <span>Минуты</span>
+        </label>
+        <label>
+          <input 
+            type="radio" 
+            value="monthly" 
+            checked={scheduleType === 'monthly'} 
+            onChange={() => setScheduleType('monthly')} 
+          />
+          <span>Месяцы</span>
+        </label>
+      </div>
     </div>
   );
 }
